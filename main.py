42ef43d29547a3d84e0dd30eea9a27ca2d7d4779
chris <chrisrossell1999@gmail.com>
2023-07-19 12:46:27 +0100

    Loaded excel data into data frames

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from fpdf import FPDF
 import pandas as pd
+import glob
 
+# = everything ending in xlsx
+filepaths = glob.glob("Invoices/*xlsx")
+
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
